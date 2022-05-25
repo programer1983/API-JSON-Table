8773f8da5ec4f2a4185949b3bfb0e30c24b71185
programer1983 <programer972@gmail.com>
2022-05-25 11:58:17 +0300

    deleting unnecessary files adding hooks;

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from "react"
 
 function App() {
+  const API_URL = 'https://jsonplaceholder.typicode.com/'
+  const [reqType, setReqType] = useState('users')
+  const [items, setItems] = useState([])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
     </div>
   );
 }
